1d1fea4a80363de94e8ccea266c506e5bc3109ff
SJArpita <rsheea12@gmail.com>
2025-04-19 13:01:02 +0800

    initial commit

--- ecr.tf
+++ ecr.tf
@@ -1,17 +1,17 @@
 
-resource "aws_ecr_repository" "example" {
-  name                 = "my-ecr-repo" # Replace with your desired repository name
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-  image_tag_mutability = "MUTABLE"
-  tags = {
-    Environment = "Production"
-    Team        = "DevOps"
-  }
-}
+# resource "aws_ecr_repository" "example" {
+#   name                 = "my-ecr-repo" # Replace with your desired repository name
+#   image_scanning_configuration {
+#     scan_on_push = true
+#   }
+#   image_tag_mutability = "MUTABLE"
+#   tags = {
+#     Environment = "Production"
+#     Team        = "DevOps"
+#   }
+# }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.example.repository_url
-  description = "The URL of the created ECR repository"
-}
+# output "ecr_repository_url" {
+#   value = aws_ecr_repository.example.repository_url
+#   description = "The URL of the created ECR repository"
+# }
--- ecs.tf
+++ ecs.tf
@@ -1,18 +1,18 @@
 
 
-# Create an ECS Cluster
-resource "aws_ecs_cluster" "example" {
-  name = "my-ecs-cluster" # Replace with your desired cluster name
-  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+# # Create an ECS Cluster
+# resource "aws_ecs_cluster" "example" {
+#   name = "my-ecs-cluster" # Replace with your desired cluster name
+#   capacity_providers = ["FARGATE", "FARGATE_SPOT"]
 
-  tags = {
-    Environment = "Production"
-    Team        = "DevOps"
-  }
-}
+#   tags = {
+#     Environment = "Production"
+#     Team        = "DevOps"
+#   }
+# }
 
-# Optional: Output the ECS Cluster Name
-output "ecs_cluster_name" {
-  value       = aws_ecs_cluster.example.name
-  description = "The name of the ECS cluster"
-}
+# # Optional: Output the ECS Cluster Name
+# output "ecs_cluster_name" {
+#   value       = aws_ecs_cluster.example.name
+#   description = "The name of the ECS cluster"
+# }
